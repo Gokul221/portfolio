53dd40b32ca7738009668c8fb98564c35b275bab
Gokul <54464084+Gokul221@users.noreply.github.com>
2025-03-14 00:30:22 +0530

    add robots.txt and sitemap.xml

--- app/layout.js
+++ app/layout.js
@@ -24,6 +24,14 @@ export default function RootLayout({ children }) {
     <html lang="en" className="scroll-smooth dark">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <meta name="keywords" content={metadata.keywords} />
+        <meta name="author" content={metadata.author} />
+        <meta property="og:title" content={metadata.title} />
+        <meta property="og:description" content={metadata.description} />
+        <meta property="og:image" content="/portfolio_ss.png" />
+        <meta property="og:url" content="https://gokul-bhoi-portfolio.netlify.app/" />
       </head>
       <body
         className={`${outfit.className} ${ovo.className} antialiased leading-8 overflow-x-hidden dark:bg-darkTheme dark:text-white`}
